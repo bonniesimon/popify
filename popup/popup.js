7ba50b8d7869041b75b77ce08d759c898d543c2c
bonnie <bonnie.developer@gmail.com>
2020-07-13 18:26:08 +0530

    got current tab details😪

--- popup/popup.js
+++ popup/popup.js
@@ -4,7 +4,15 @@ const runExt = () => {
     popBtn.addEventListener("click", () => {
         window.open("https:open.spotify.com", "spotify", "fullscreen=yes");
     });
+    browser.tabs.query({currentWindow: true, active:true}, logTabs);
 }
 
+const logTabs = (tabs) => {
+    console.log(tabs[0].url);
+}
+
+// const getCurrentTab = () => {
+
+// }
 
 runExt();
