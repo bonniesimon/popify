19f25148800241fe5961f5c176caf4b1cd378ba3
bonnie <bonnie.developer@gmail.com>
2020-07-13 18:34:29 +0530

    Can't seem to return

--- popup/popup.js
+++ popup/popup.js
@@ -4,15 +4,24 @@ const runExt = () => {
     popBtn.addEventListener("click", () => {
         window.open("https:open.spotify.com", "spotify", "fullscreen=yes");
     });
-    browser.tabs.query({currentWindow: true, active:true}, logTabs);
+    const urlTab = getCurrentUrl();
+    console.log(urlTab);
 }
 
-const logTabs = (tabs) => {
-    console.log(tabs[0].url);
+//* Function to get Current tab object
+const getCurrentTab = () => {
+    return browser.tabs.query({currentWindow: true, active:true});
 }
 
-// const getCurrentTab = () => {
-
-// }
+//* Function to get current tab url
+const getCurrentUrl = () => {
+    let urlTab;
+    getCurrentTab().then((tabs) => {
+        // console.log(tabs[0].url)
+        urlTab = tabs[0].url;
+    })
+    console.log(urlTab);
+    return urlTab; 
+}
 
 runExt();
